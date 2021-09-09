a064a46e1c4a99fe0977b0ee4a2321ce5ddcd5ae
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-09-09 21:13:52 +0530

    style : stylesheet not loading.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('styles'))
+app.use(express.static('public'))
 app.set("view engine","ejs")
 
 
