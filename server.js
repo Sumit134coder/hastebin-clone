5863e27a6a9e5fd5d2ec6e8940e0c10a5a407e41
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-09-15 17:45:06 +0530

    feat : save route , views

--- server.js
+++ server.js
@@ -4,16 +4,31 @@ const PORT = 3000;
 
 app.use(express.static('public'))
 app.set("view engine","ejs")
+app.use(express.urlencoded({ extended: true }))
 
+//model middleware
+const Document= require("./models/Document")
+const mongoose = require("mongoose")
+mongoose.connect()
 
 app.get("/",(req,res)=>{
-    const code = `This is the code now on you can add whatecer you want \nhello
-now whahrk adjdfsf`
-    res.render('code-display' , {code})
+    const code = `Welcome to YourBin`
+    res.render('code-display' , {code,language:"plaintext"})
 })
 
 app.get("/new",(req,res)=>{
     res.render('new')
+})
+
+app.post("/save" , async(req,res)=>{
+    const value = req.body.value;
+    try{
+        const document = await Document.create({value})
+        res.redirect(`/${document.id}`)
+
+    }catch(e){
+        res.render("new",{value})
+    }
 })
 
 app.listen(PORT , ()=>{
